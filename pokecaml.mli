fefcfe4a5a687c8c622e4e5469043fe487b15541
hdunn <hld59@cornell.edu>
2015-11-17 21:26:17 -0500

    Refactored pokecaml.mli and main.mli

---
+++ pokecaml.mli
@@ -0,0 +1,9 @@
+(** A record containing a string of its name, a list of attacks, a string of its
+  * type, and an int HP *)
+type pokecaml
+
+(** A list containing all of the trainer’s pokecaml*)
+val camldex : pokecaml list
+
+(** A list of all pokecaml in the game*)
+val all_pokecaml : pokecaml list
