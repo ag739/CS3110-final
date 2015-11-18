1a63000c8ee9f624d91c47445ad78db0477e5059
Ariel <ag739@cornell.edu>
2015-11-17 23:25:08 -0500

    added some stuff to wild battle

--- wild_pokecaml_battle.ml
+++ wild_pokecaml_battle.ml
@@ -42,9 +42,29 @@ let rec first_pokecaml camldex =
   | [] -> failwith "should have a pokecaml in the camldex"
   | h::t -> if h.hp > 0 then h else first_pokecaml t
 
+let rec print_attacks (attacks : attack list) : unit =
+  match attacks with
+  | [] -> print_newline ()
+  | (a,_)::t -> let () = print_string (a ^ "\n") in print_attacks t
+
 let rec battle (camldex : pokecaml list) (wild : pokecaml) (player: int) : unit =
   (* If it is our player, print out possible moves or the option to try to catch. Input must be one of these options or they need to input again. After input, print out damage or result of catch *)
 (* If it is not our player, print out what move the wild pokecaml is playing and the damage *)
-
-  let input = String.lowercase (read_line ()) in
-  failwith ("TODO"^input)
+  if player = 0 then
+    let current_pokecaml = first_pokecaml camldex in
+    let () = print_string "It's your turn! What will you do?\n
+                           Type 'catch' to catch the wild pokecaml.\n
+                           Or, you can type any of your attacks:" in
+    let () = print_attacks current_pokecaml.attacks in
+    let input = String.lowercase (read_line ()) in
+    if input = "catch" then
+      if (catch wild) = true then
+        (* TODO: update your camldex and end the battle*)
+      else battle camldex wild 1
+    else
+      (* TODO: make sure input is a valid attack *)
+      battle camldex wild 1
+  else
+    (* TODO: randomly pick the wild pokecaml's attack and adjust hp *)
+    let () = print_string (wild.name ^ " attacked with ") in
+    battle camldex wild 0
