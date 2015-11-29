1495eac1162340a60d37644ed6049980a00ec07e
Ariel <ag739@cornell.edu>
2015-11-28 20:05:42 -0500

    added intro in main

--- main.ml
+++ main.ml
@@ -33,7 +33,7 @@ let rec game (camldex: pokecaml list) : unit =
   let () = print_string ">>> " in
   let input = read_line () in
   match find_command input with
-  | Quit -> let () = print_endline "Goodbye!" in exit 0
+  | Quit -> let () = print_endline "I guess you won't be the next Pokecaml master...goodbye!" in exit 0
   | Camldex -> let () = print_endline (print_camldex camldex) in game camldex
   | Help -> let () = print_endline "Possible commands are:\n
                    \"Battle\" to fight an opponent\n
@@ -60,9 +60,25 @@ let rec first_camldex input =
                   pokecaml_type = Humanities; hp= 100}]
   | _ -> let () = print_string "Please try again, Professor Michael \"Oak\" Clarkson does not have that Pokecaml!\n\n>>> " in first_camldex (read_line ())
 
-
-
 let () =
-  let () = print_endline "Welcome to Pokecaml! Catchy text goes here!!" in
-  let () = print_string "You can choose between Recursee, a Software Pokecaml, Deferredata, a Hardware Pokecaml, or Proofle, a Humanities Pokemon. Which do you pick?\n\n>>> " in
+  let () = print_string "  Hello! I've been waiting for you.\n  ...
+  Oh, my name? Professor Michael \"Oak\" Clarkson. I'm assuming you've heard
+  about the...situation.\n  ...\n  No? Oh, do we have some catching up to do!
+  Basically, one of the project team's robotic PokeCaml experiments have gone
+  horribly wrong, so Pokecaml have gone rogue and are all over Gates Hall.
+  The project team members decided to go with it, and have also gone rogue,
+  collecting Pokecaml, and in general harassing the student population.
+  Some may say that Cornell is now scarier than having three back to back
+  prelims during Fall Break. I believe you can be the very best functional
+  Pokecaml trainer; you are one of my top students in CS3110, after all.
+  I'll give you unlimited CamlBalls and your first Pokecaml.
+  I'll be here to help you out. Remember the commands I taught you in class:
+                   \"Battle\" to fight an opponent
+                   \"Camldex\" to see your Camldex
+                   \"Quit\" to end the game forever
+  I have three Pokecaml, but I can only give you one:
+                  Recursee, a Software Pokecaml
+                  Deferredata, a Hardware Pokecaml
+                  Proofle, a Humanities Pokecaml
+  Which do you choose? \n>>> " in
   game (first_camldex (read_line ()))
